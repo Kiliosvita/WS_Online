4d8dd886699b5206cce89c2a6bba0f0907f123f0
Kiliosvita <williamxu1998@gmail.com>
2020-04-25 20:55:46 -0400

    Add files via upload

---
+++ Shuffle 2/Zones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shuffle_2
+{
+    public class Zones
+    {
+        
+       // public bool occupied { get; set; }
+        //state will be used for rested, reversed, flipped
+       // public int state { get; set; }
+       // public Card[] zoneMembers { get; set; }
+       // public PictureBox areas { get; set; }
+       // public Zones(bool occupied, int state, Card[] zoneMembers, PictureBox areas, int position)
+      //  {
+       //     this.occupied = occupied;
+       //     this.state = state;
+      //      this.zoneMembers = zoneMembers;
+      //      this.areas = areas;
+      //  }
+
+    }
+}
